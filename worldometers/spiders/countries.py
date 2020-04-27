56208ee99b35e154fb1687a1683e1a861e1516d5
Bex Tuychiev <b3832570@gmail.com>
2020-04-27 23:33:07 +0500

    Add feature to return population for each year

--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import logging
 
 
 class CountriesSpider(scrapy.Spider):
@@ -10,8 +11,21 @@ class CountriesSpider(scrapy.Spider):
     def parse(self, response):
         countries = response.xpath('//td/a')
         for country in countries:
-            name = country.xpath('.//text()').get()
             link = country.xpath('.//@href').get()
             # absolute_url = response.urljoin(link)
 
-            yield response.follow(link)
+            yield response.follow(link, callback=self.parse_countries)
+
+    def parse_countries(self, response):
+        rows = response.xpath(
+            "(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]//tr["
+            "position() > 1]")
+        for row in rows:
+            year = row.xpath('.//td[1]/text()').get()
+            population = row.xpath('.//td[2]/strong/text()').get()
+
+            yield {
+                'country_link': response.url,
+                'year': year,
+                'population': population
+            }
